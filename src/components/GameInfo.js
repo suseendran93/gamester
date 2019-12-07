dc631f3526a6021b8b5de56674b640b1efb2101f
Bharathikannan <bharathikannan@live.com>
2019-12-07 11:41:02 +0530

    catagoryRevamp
    catagoryRevamp

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -6,21 +6,23 @@ class Gameinfo extends Component{
     constructor(){
         super();
         this.showInfo = this.showInfo.bind(this);
-
-        window.onload=(function load(){
-           Category.hideInfo();
-            var games=document.querySelectorAll('.drop li');
-            for(var i=0;i<games.length;i++){
-            games[i].addEventListener("click",this.showInfo);
-        }
-        
-        })();
     }
 
-    
+    // findList(){
+    //     this.props.sendPram
+    // }
+
+    // hideInfo(){
+	
+    //     		document.getElementById("close").style.display='none';
+    //     		document.getElementById("RPG").style.display='none';
+    //     		document.getElementById("FPS").style.display='none';
+    //     		document.getElementById("Racing").style.display='none';
+    //     		document.getElementById("Free to play").style.display='none';
+    //     	}
      showInfo(){
 
-        Category.hideInfo();
+        // Category.hideInfo();
         document.getElementById("close").style.display='block';
         var text = this.children[0].innerText;
         
@@ -50,10 +52,11 @@ class Gameinfo extends Component{
     render(){
         return(
             <div className="gameInfo">
-        <div id="close" class="close-content">
+                {this.props.sendPram}
+        <div id="close" class="close-content displayNone">
         <button  type="button" onclick="hideInfo()">X</button>
         </div>
-        <div id="RPG" class="gameCategory">
+        <div id="RPG" class="gameCategory displayNone">
             <h2>Role playing games</h2>
             <p>A role-playing game (sometimes spelled roleplaying game; abbreviated RPG) is a game in which players assume the roles of characters in a fictional setting. Players take responsibility for acting out these roles within a narrative, either through literal acting, or through a process of structured decision-making regarding character development.Actions taken within many games succeed or fail according to a formal system of rules and guidelines.
             </p>
@@ -62,13 +65,13 @@ class Gameinfo extends Component{
             <p>Several varieties of RPG also exist in electronic media, such as multiplayer text-based Multi-User Dungeons (MUDs) and their graphics-based successors, massively multiplayer online role-playing games (MMORPGs). Role-playing games also include single-player role-playing video games in which players control a character, or team of characters, who undertake(s) quests, and may include player capabilities that advance using statistical mechanics. These electronic games sometimes share settings and rules with tabletop RPGs, but emphasize character advancement more than collaborative storytelling.</p>
             <p>This type of game is well-established, so some RPG-related game forms, such as trading/collectible card games (CCGs) and wargames, may not be included under the definition. Some amount of role-playing activity may be present in such games, but it is not the primary focus.The term role-playing game is also sometimes used to describe games involving roleplay simulation and exercises used in teaching, training, and academic research.</p>
             </div>
-        <div id="FPS" class="gameCategory">
+        <div id="FPS" class="gameCategory displayNone">
             <h2>First person shooting</h2>
         </div>
-        <div id="Racing" class="gameCategory">
+        <div id="Racing" class="gameCategory displayNone">
             <h2>Racing</h2>
         </div>
-        <div id="Free to play" class="gameCategory">
+        <div id="Free to play" class="gameCategory displayNone">
             <h2>Free to play</h2>
         </div>
         </div>
