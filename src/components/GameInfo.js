cfa2d55bf816e39f2518c8d9d262c2955bfa186f
Suseendran,Kumar <1601919@zone1.scb.net>
2019-12-17 20:50:30 +0530

    Jsons added

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import '../styles/Gameinfo.css';
-
+import GameContent from '../Jsons/GameContent';
+const categoryContent= GameContent.category;
 class Gameinfo extends Component{
     constructor(props){
         super(props);
+         
          this.hideInfo = this.hideInfo.bind(this);
-        this.state = {
+         this.state = {
             flag1 : false,
             // flagX: this.props.sendPram.flag
         }
@@ -31,13 +33,12 @@ class Gameinfo extends Component{
     render(){
         console.log(this.state.flagX);
         let {listVal}= this.props.sendPram;
-        // console.log(listVal);
         return(
         <div className="gameInfo">
         <div id="close" className={!this.state.flag1 ? "close-content displayBlock" : "close-content displayNone"}>
         <button  type="button" onClick={this.hideInfo}>X</button>
         </div>
-        <div className = {!this.state.flagX ? "gameCategory displayBlock" : "gameCategory displayNone" }>
+        <div className = {!this.state.flag1 ? "gameCategory displayBlock" : "gameCategory displayNone" }>
         {listVal === "RPG" &&
         (<><h2>Role playing games</h2>
         <p>A role-playing game (sometimes spelled roleplaying game; abbreviated RPG) is a game in which players assume the roles of characters in a fictional setting. Players take responsibility for acting out these roles within a narrative, either through literal acting, or through a process of structured decision-making regarding character development.Actions taken within many games succeed or fail according to a formal system of rules and guidelines.
@@ -47,9 +48,11 @@ class Gameinfo extends Component{
         <p>Several varieties of RPG also exist in electronic media, such as multiplayer text-based Multi-User Dungeons (MUDs) and their graphics-based successors, massively multiplayer online role-playing games (MMORPGs). Role-playing games also include single-player role-playing video games in which players control a character, or team of characters, who undertake(s) quests, and may include player capabilities that advance using statistical mechanics. These electronic games sometimes share settings and rules with tabletop RPGs, but emphasize character advancement more than collaborative storytelling.</p>
         <p>This type of game is well-established, so some RPG-related game forms, such as trading/collectible card games (CCGs) and wargames, may not be included under the definition. Some amount of role-playing activity may be present in such games, but it is not the primary focus.The term role-playing game is also sometimes used to describe games involving roleplay simulation and exercises used in teaching, training, and academic research.</p></>)
         || listVal === "FPS" &&
-
-        (<><h2>First person shooting</h2></>)
-
+        (<div
+            dangerouslySetInnerHTML={{
+          __html: categoryContent[0].RPG
+        }}></div>)
+        
         || listVal === "Racing" &&
     
         (<><h2>Racing</h2></>)
