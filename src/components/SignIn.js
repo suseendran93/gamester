74c145cbfcff150b08fd4d597dd44c72a98466fc
suseendran93 <ssndrn@gmail.com>
2019-12-29 19:20:22 +0530

    Added form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -20,7 +20,7 @@ class SignIn extends Component {
             toggle: !this.state.toggle
         })
     }
-    openCreateAccountWindow(){
+    openCreateAccountWindow() {
         this.setState({
             toggleAccount: !this.state.toggleAccount
         })
@@ -30,26 +30,30 @@ class SignIn extends Component {
             <div className="cta">
                 <div className="btn-container">
                     <button id="signin" onClick={this.openSignInWindow}>Sign in</button>
+                    <form className="signin-form">
                     <div id="overlay" className={this.state.toggle ? "signin-screen-overlay displayBlock" : "signin-screen-overlay displayNone"}>
+                    
                         <div className={this.state.toggle ? "signin-screen displayBlock" : "signin-screen displayNone"}>
+                            
+                                <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
+                                <div className="username">
+                                    <input id="username" type="text" name="username" placeholder="Username" required />
+                                </div>
+                                <div className="password">
+                                    <input id="password" type="password" name="password" placeholder="Password" required />
+                                </div>
+                                <div className="login">
+                                    <button type="submit" id="login">Log in</button>
+                                </div>
+                                <div className="create-account">
+                                    <button id="createAccount" onClick={this.openCreateAccountWindow.bind(this)} >Create a new account</button>
 
-                            <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
-                            <div className="username">
-                                <input id="username" type="text" name="username" placeholder="Username" required />
-                            </div>
-                            <div className="password">
-                                <input id="password" type="password" name="password" placeholder="Password" required />
-                            </div>
-                            <div className="login">
-                                <button id="login">Log in</button>
-                            </div>
-                            <div className="create-account">
-                                <button id="createAccount" onClick={this.openCreateAccountWindow.bind(this)} >Create a new account</button>
-                                
-                            </div>
-
+                                </div>
+                           
                         </div>
+                      
                     </div>
+                    </form>
                 </div>
             </div>
         )
