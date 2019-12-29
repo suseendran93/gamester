f87d081dd0eb494dc091ae26bfd79b8b9b1440df
suseendran93 <ssndrn@gmail.com>
2019-12-29 18:51:09 +0530

    Added create account component

---
+++ src/components/CreateAccount.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import '../styles/CreateAccount.css';
+
+class CreateAccount extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+}
+
+export default CreateAccount;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,50 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/SignIn.css';
 import '../styles/Category.css';
+import CreateAccount from '../components/CreateAccount';
 
-class SignIn extends Component{
+class SignIn extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.openSignInWindow = this.openSignInWindow.bind(this);
-        this.state={
-            toggle: null
+        this.state = {
+            toggle: null,
+            toggleAccount: null
 
         }
     }
 
-    openSignInWindow(){
-            this.setState({
-                toggle: !this.state.toggle
-            })
+    openSignInWindow() {
+        this.setState({
+            toggle: !this.state.toggle
+        })
     }
-    render(){
-    return(
-        <div className="cta">
-            <div className="btn-container">
-            <button id="signin" onClick={this.openSignInWindow}>Sign in</button>
-            <div id="overlay" className={this.state.toggle ?"signin-screen-overlay displayBlock":"signin-screen-overlay displayNone"}>
-                <div className={this.state.toggle ?"signin-screen displayBlock":"signin-screen displayNone"}>
-                
-                <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
-                <div className="username">
-                    <input id="username" type="text" name="username" placeholder="Username" required/>
+    openCreateAccountWindow(){
+        this.setState({
+            toggleAccount: !this.state.toggleAccount
+        })
+    }
+    render() {
+        return (
+            <div className="cta">
+                <div className="btn-container">
+                    <button id="signin" onClick={this.openSignInWindow}>Sign in</button>
+                    <div id="overlay" className={this.state.toggle ? "signin-screen-overlay displayBlock" : "signin-screen-overlay displayNone"}>
+                        <div className={this.state.toggle ? "signin-screen displayBlock" : "signin-screen displayNone"}>
+
+                            <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
+                            <div className="username">
+                                <input id="username" type="text" name="username" placeholder="Username" required />
+                            </div>
+                            <div className="password">
+                                <input id="password" type="password" name="password" placeholder="Password" required />
+                            </div>
+                            <div className="login">
+                                <button id="login">Log in</button>
+                            </div>
+                            <div className="create-account">
+                                <button id="createAccount" onClick={this.openCreateAccountWindow.bind(this)} >Create a new account</button>
+                                
+                            </div>
+
+                        </div>
                     </div>
-                    <div className="password">
-                    <input id="password" type="password" name="password" placeholder="Password" required/>
-                </div>
-                <div className="login">
-                <button id="login" >Log in</button>
-                </div>
-                <div className="create-account">
-                    <a href="#/">Create a new account</a>
-                </div>
-            
-                </div>
                 </div>
             </div>
-        </div>
-    )
+        )
     }
 }
 
