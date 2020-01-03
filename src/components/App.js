bbdc97936dd8051f30e63aec76c08397b811d44d
Suseendran,Kumar <1601919@zone1.scb.net>
2020-01-03 18:41:21 +0530

    Added webpack with error

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
+// import '../styles/App.css';
 import Navbar from './Navbar'
 import SignIn from './SignIn';
 import Category from './Category';
+
 class App extends Component {
 
   render(){
@@ -13,7 +14,7 @@ class App extends Component {
         <div className="fixed-headers">
 	        <div className="header">
 		        <h1>Welcome to Gaming info</h1>
-		        <h5>-Everything you need to game</h5>
+		        <h5>Everything you need to game</h5>
 	        </div>
 	        <div className="searchbar">
 	        	<input type="text" name="searchbar" placeholder="Search..."/>
