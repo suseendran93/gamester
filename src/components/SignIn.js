604d78e272c14c654297cec4145e028336c73274
suseendran93 <ssndrn@gmail.com>
2020-01-04 16:52:26 +0530

    Added webpack successfully

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -30,31 +30,30 @@ class SignIn extends Component {
             <div className="cta">
                 <div className="btn-container">
                     <button id="signin" onClick={this.openSignInWindow}>Sign in</button>
+                    <form className="signin-form">
+                        <div id="overlay" className={this.state.toggle ? "signin-screen-overlay displayBlock" : "signin-screen-overlay displayNone"}>
 
-                    <div id="overlay" className={this.state.toggle ? "signin-screen-overlay displayBlock" : "signin-screen-overlay displayNone"}>
+                            <div className={this.state.toggle ? "signin-screen displayBlock" : "signin-screen displayNone"}>
 
-                        <div className={this.state.toggle ? "signin-screen displayBlock" : "signin-screen displayNone"}>
+                                <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
+                                <div className="username">
+                                    <input id="username" type="text" name="username" placeholder="Username" required />
+                                </div>
+                                <div className="password">
+                                    <input id="password" type="password" name="password" placeholder="Password" required />
+                                </div>
+                                <div className="login">
+                                    <button type="submit" id="login">Log in</button>
+                                </div>
+                                <div className="create-account">
+                                    <button id="createAccount" onClick={this.openCreateAccountWindow.bind(this)} >Create a new account</button>
 
-                            <button id="close-signin" onClick={this.openSignInWindow}>Close</button>
-                            <div className="username">
-                                <input id="username" type="text" name="username" placeholder="Username" required />
-                            </div>
-                            <div className="password">
-                                <input id="password" type="password" name="password" placeholder="Password" required />
-                            </div>
-                            <div className="login">
-                                <button type="submit" id="login">Log in</button>
-                            </div>
-                            <div className="create-account">
-                                <button id="createAccount" onClick={this.openCreateAccountWindow.bind(this)} >Create a new account</button>
-                                <CreateAccount {...this.state}/>   
+                                </div>
+
                             </div>
 
                         </div>
-
-                    </div>
-
-
+                    </form>
                 </div>
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -20,17 +20,30 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-
+                exclude: /node_modules/,
+                use: ['style-loader',//3.Inject styles into DOM
+                    'css-loader',   //2.Turns css into common js(Look inside bundle.js)
+                    'sass-loader'   //1. Turns sass into css
+                ]
             },
-
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'file-loader'
+                }
+            }
         ],
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './public/index.html'
-            }),
-            new MiniCssExtractPlugin()
-        ]
 
-    }
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin(
+            {
+            }
+        )
+    ]
 }
